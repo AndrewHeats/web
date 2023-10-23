dbf5c8930e0e080bfa9edd403c89ad8375d96111
Andrew Hearts <andreyko.era@gmail.com>
2023-10-23 19:48:12 +0300

    fixing issues with app.js file

---
+++ web/src/components/zoos/zoos.js
@@ -0,0 +1,27 @@
+
+function Zoos(){
+    return (
+        <div className="container">
+            
+            <li className="zooitem">
+                <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/e1/f7/d3/visit-wildlights-at-the.jpg?w=500&h=400&s=1" className="zooimage"/>
+                <h4 className="zooheading">Ohio zoo</h4>
+                <p className="zooinfo">The Columbus Zoo and Aquarium is located in Powell, Ohio, just north of Columbus. It is one of the largest and most famous zoos in the United States. The zoo is known for its diverse collection of animals, conservation efforts, and educational programs.</p>
+            </li>
+            <li className="zooitem">
+                <img src="https://cdn.britannica.com/14/142114-050-2DEBE25C/entrance-Elephant-Gate-Berlin-Zoological-Garden-and.jpg" className="zooimage"/>
+                <h4 className="zooheading">Berlin zoo</h4>
+                <p className="zooinfo">The Berlin Zoo, officially known as the "Zoologischer Garten Berlin" in German, is one of the most famous and historic zoos in the world. Here are some key details about the Berlin Zoo:</p>
+            </li>
+            <li className="zooitem">
+                <img src="https://images.adsttc.com/media/images/5420/e064/c07a/800d/e500/0092/large_jpg/ARM131211-02.jpg?1411440729" className="zooimage"/>
+                <h4 className="zooheading">Paris zoo</h4>
+                <p className="zooinfo">The Paris Zoological Park, commonly known as the "Parc Zoologique de Paris" or "Zoo de Vincennes," is a prominent zoo located in the eastern part of Paris, France. Here's some information about the Paris Zoo:</p>
+            </li>
+            
+            
+        </div>
+    );
+}
+
+export default Zoos
