f0a95d3724721ce33153034d5fa56c4a1391fd87
Andrew Hearts <andreyko.era@gmail.com>
2023-11-17 09:33:12 +0200

    Adding loading

--- web/ninth_lab/client/src/components/loading/Loading.js
+++ web/ninth_lab/client/src/components/loading/Loading.js
@@ -1,5 +1,9 @@
 function Loading(){
     return <div className="Loading">
+        <div class="loader-wrapper">
+            <div class="loader"></div>
+            <p>Loading...</p>
+        </div>
         <h1 className="LoadingText">The data is loading please wait</h1>
     </div>
 }
